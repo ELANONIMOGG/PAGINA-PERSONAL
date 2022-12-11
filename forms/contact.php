ca9a1b64358ccd81e315421c9e5d1d4f6e74ea19
Miguel Zamudio <94863947+ELANONIMOGG@users.noreply.github.com>
2022-12-11 06:47:02 +0000

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -1,19 +1,17 @@
 <?php
-
-$receiving_email_address = 'mzo2108s@gmail.com';
-$subject = $_POST['subject'];
-$message = $_POST['message'];
-
-$headers = 'From:' . $_POST['email'] .
-  "\r\n" . 'Reply-To:' . $_POST['email'] .
-  "\r\n" . 'X-Mailer: PHP/' . phpversion() .
-  "\r\n" . 'Content-Type: text/plain; charset=utf-8';
-
-$success = mail($receiving_email_address, $subject, $message, $headers);
-if (!$success) {
-  $errorMessage = error_get_last()['message'];
-} else {
-  echo "OK";
-}
-
-?>
+if(isset($_POST['email'])) {
+ 
+    // Debes editar las próximas dos líneas de código de acuerdo con tus preferencias
+    $email_to = "Contactoelanonimo1@gmail.com";
+    $email_subject = "Contacto desde el sitio web";
+ 
+    // Aquí se deberían validar los datos ingresados por el usuario
+    if(!isset($_POST['first_name']) ||
+        !isset($_POST['last_name']) ||
+        !isset($_POST['email']) ||
+        !isset($_POST['comments'])) {
+ 
+        echo "<b>Ocurrió un error y el formulario no ha sido enviado. </b><br />";
+        echo "Por favor, vuelva atrás y verifique la información ingresada<br />";
+        die();
+    }
